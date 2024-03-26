2df51919c530341747d790ced058d6f12e69b1e9
schen <5460009+toshichi@users.noreply.github.com>
2024-03-26 21:02:58 +0900

    add: bindkeys

--- template/rc.sh
+++ template/rc.sh
@@ -1,1 +1,6 @@
 # processing for all platforms
+bindkey '^H' backward-kill-word
+bindkey '^[[3;5~' kill-word
+bindkey  "^[[H"   beginning-of-line
+bindkey  "^[[F"   end-of-line
+bindkey  "^[[3~"  delete-char
