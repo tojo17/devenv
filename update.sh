13dc5d277807420a06fbfa5cdcc7de318553017e
schen <5460009+toshichi@users.noreply.github.com>
2024-03-26 20:42:35 +0900

    fix: windows CRLF for wslpath

--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ echo "" >> $OUTFILE
 if grep -q Microsoft /proc/version; then
     echo "WSL detected"
     # get windows home path
-    WIN_HOME="$(wslpath $(powershell.exe -NoProfile -NonInteractive -Command "\$Env:UserProfile"))" 
+    WIN_HOME="$(wslpath $(powershell.exe -NoProfile -NonInteractive -Command "\$Env:UserProfile" | tr -d '\r'))" 
     echo "Windows home path: $WIN_HOME"
     # replace {[win_home]} with $WIN_HOME for wsl.sh, and append to $OUTFILE
     sed "s|{\[win_home\]}|$WIN_HOME|g" $SCRIPT_DIR/wsl.sh >> $OUTFILE
