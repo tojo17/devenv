d3b9e4ba2c7ef0fe8f5ead68c26a887d76e47b94
schen <5460009+toshichi@users.noreply.github.com>
2024-03-26 20:00:01 +0900

    mod: on error exit

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# git zsh should be already installed
+# on error exit
+set -e
+
+# git should be already installed
 git clone https://github.com/toshichi/devenv.git ~/devenv
 
 # run update.sh to generate rc file
@@ -10,8 +13,10 @@ source ~/devenv/update.sh
 # if bash
 if [ -n "$BASH_VERSION" ]; then
     echo "source ~/devenv/rc.sh" >> ~/.bashrc
+    echo "added source ~/devenv/rc.sh to ~/.bashrc"
 fi
 # if zsh
 if [ -n "$ZSH_VERSION" ]; then
     echo "source ~/devenv/rc.sh" >> ~/.zshrc
+    echo "added source ~/devenv/rc.sh to ~/.zshrc"
 fi
