1cbd80f82c3195726f30e2cf43ad70d43b9a47b7
schen <5460009+toshichi@users.noreply.github.com>
2024-03-26 20:19:57 +0900

    fix: stop processing on error

--- install.sh
+++ install.sh
@@ -9,16 +9,16 @@
 
     # run update.sh to generate rc file
     source ~/devenv/update.sh
-)
 
-# add rc.sh to current shell rc
-# if bash
-if [ -n "$BASH_VERSION" ]; then
-    echo "source ~/devenv/rc.sh" >> ~/.bashrc
-    echo "added source ~/devenv/rc.sh to ~/.bashrc"
-fi
-# if zsh
-if [ -n "$ZSH_VERSION" ]; then
-    echo "source ~/devenv/rc.sh" >> ~/.zshrc
-    echo "added source ~/devenv/rc.sh to ~/.zshrc"
-fi
+    # add rc.sh to current shell rc
+    # if bash
+    if [ -n "$BASH_VERSION" ]; then
+        echo "source ~/devenv/rc.sh" >> ~/.bashrc
+        echo "added source ~/devenv/rc.sh to ~/.bashrc"
+    fi
+    # if zsh
+    if [ -n "$ZSH_VERSION" ]; then
+        echo "source ~/devenv/rc.sh" >> ~/.zshrc
+        echo "added source ~/devenv/rc.sh to ~/.zshrc"
+    fi
+)
