b0b4a91764b89d1025ba5fab79cfafa1f88df7c7
schen <5460009+toshichi@users.noreply.github.com>
2024-03-26 21:28:15 +0900

    add: color set for wsl folders

--- template/wsl.sh
+++ template/wsl.sh
@@ -1,4 +1,12 @@
 # WSL commands
 # {[ win_home ]} is a placeholder for the Windows home directory path
+# sync ssh keys with windows
 rsync -avq --delete --chmod=600 {[win_home]}/.ssh ~
+# use windows ssh-agent
 export SSH_AUTH_SOCK={[win_home]}/ssh-agent.sock
+# change windows dir color under ls
+# dark gray for 256 terminals
+export LS_COLORS="$LS_COLORS:ow=34;48;5;233:"
+# black for ANSI terminal
+# export LS_COLORS="$LS_COLORS:ow=34;40:"
+
