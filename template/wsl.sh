7cf3d6d225e75d023f8ce680ea20d8351dec6eea
schen <5460009+toshichi@users.noreply.github.com>
2024-03-26 20:37:29 +0900

    fix: comment get replaced problem

--- template/wsl.sh
+++ template/wsl.sh
@@ -1,4 +1,4 @@
-# WSL template
-# {[win_home]} is a placeholder for the Windows home directory path
+# WSL commands
+# {[ win_home ]} is a placeholder for the Windows home directory path
 rsync -avq --delete --chmod=600 {[win_home]}/.ssh ~
 export SSH_AUTH_SOCK={[win_home]}/ssh-agent.sock
