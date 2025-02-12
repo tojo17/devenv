93b8974f7d693ddb2ed49e16661c93b8335611a5
schen <5460009+toshichi@users.noreply.github.com>
2025-02-13 01:30:01 +0900

    mod: wsl git use win ssh.exe

--- template/wsl.sh
+++ template/wsl.sh
@@ -3,7 +3,7 @@
 
 # sync ssh keys with windows
 # rsync -avq --delete --chmod=600 {[win_home]}/.ssh ~
-# use windows ssh-agent
+# proxy windows ssh-agent
 # export SSH_AUTH_SOCK={[win_home]}/ssh-agent.sock
 
 # use windows OpenSSH binaries
@@ -11,6 +11,9 @@ alias ssh-add='ssh-add.exe'
 alias ssh='ssh.exe'
 alias scp='scp.exe'
 alias sftp='sftp.exe'
+
+# use windows git
+# alias git='git.exe'
 
 # change windows dir color under ls
 # dark gray for 256 terminals
